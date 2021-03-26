e0fc35767ba29cf28f84e5c424a0dc38c9f0d681
kaibin.yang <kaibin.yang@ttpai.cn>
2021-03-26 17:07:39 +0800

    feat(build): 项目类型为 war 时跳过 install 和 deploy

---
+++ ttpai-parents-build/ttpai-parents-extension/src/main/java/com/ttpai/framework/build/extension/AbstractExtension.java
@@ -0,0 +1,78 @@
+package com.ttpai.framework.build.extension;
+
+import org.apache.maven.AbstractMavenLifecycleParticipant;
+import org.apache.maven.execution.MavenSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.annotation.Nullable;
+import java.util.Properties;
+
+/**
+ * @author kail
+ */
+public abstract class AbstractExtension extends AbstractMavenLifecycleParticipant {
+
+    protected Logger logger = LoggerFactory.getLogger(getClass());
+
+
+    /**
+     * 控制属性
+     */
+    @Nullable
+    public abstract String skipProperties();
+
+    /**
+     * 是否应该跳过
+     */
+    protected boolean shouldSkip(Properties properties) {
+        String skipConfig = properties.getProperty(skipProperties(), "false");
+        return "true".equalsIgnoreCase(skipConfig);
+    }
+
+
+    @Override
+    public void afterProjectsRead(MavenSession session) {
+        logger.info("");
+        logger.info("");
+        logger.info("===============================================================================================");
+        logger.info("ttpai-parents-extension${} is Starting...", getClass().getSimpleName());
+
+        // region 跳过逻辑
+        boolean skip = false;
+        String skipProperties = skipProperties();
+        // 有配置跳过属性
+        if (null != skipProperties && skipProperties.length() > 0) {
+            logger.info("you can use {} (true/false) to skip is extension", skipProperties());
+            //
+            // 获取跳过属性
+            skip = skip ? skip : this.shouldSkip(session.getCurrentProject().getProperties());
+            skip = skip ? skip : this.shouldSkip(session.getUserProperties());
+            skip = skip ? skip : this.shouldSkip(session.getSystemProperties());
+            if (skip) {
+                logger.info("{}=true,  will Skip", skipProperties());
+            }
+        }
+        // endregion
+
+        //
+        if (!skip) {
+            logger.info("");
+            this.afterProjectsReadPretty(session);
+            logger.info("");
+        }
+        //
+        //
+        logger.info("ttpai-parents-extension${} is End...", getClass().getSimpleName());
+        logger.info("===============================================================================================");
+        logger.info("");
+        logger.info("");
+    }
+
+
+    /**
+     * @see AbstractMavenLifecycleParticipant#afterProjectsRead(MavenSession)
+     */
+    public abstract void afterProjectsReadPretty(MavenSession session);
+
+}
--- ttpai-parents-build/ttpai-parents-extension/src/main/java/com/ttpai/framework/build/extension/WarSkipDeployExtension.java
+++ ttpai-parents-build/ttpai-parents-extension/src/main/java/com/ttpai/framework/build/extension/WarSkipDeployExtension.java
@@ -5,35 +5,59 @@ import org.apache.maven.execution.MavenSession;
 import org.apache.maven.project.MavenProject;
 import org.codehaus.plexus.component.annotations.Component;
 
+import javax.annotation.Nullable;
+import java.util.List;
+import java.util.Properties;
+
 /**
  * @author zichao.zhang
  */
 @Component(role = AbstractMavenLifecycleParticipant.class, hint = "deploy")
-public class WarSkipDeployExtension extends AbstractMavenLifecycleParticipant {
+public class WarSkipDeployExtension extends AbstractExtension {
 
     /**
      * 可配置属性
      */
     private static final String SKIP_PROPERTIES = "ttpai.parents.extension.war.skip";
 
+    @Nullable
     @Override
-    public void afterProjectsRead(MavenSession session) {
-        MavenProject currentProject = session.getCurrentProject();
+    public String skipProperties() {
+        return SKIP_PROPERTIES;
+    }
+
+    @Override
+    public void afterProjectsReadPretty(MavenSession session) {
+        // 处理所有模块
+        List<MavenProject> allProjects = session.getAllProjects();
+        for (MavenProject currentProject : allProjects) {
+            projectHandler(currentProject);
+        }
+
+    }
+
+    /**
+     * 处理项目配置
+     * <p>
+     * session.getCurrentProject() 只有 父 pom 会执行
+     */
+    protected void projectHandler(MavenProject currentProject) {
+        String projectName = currentProject.getName();
+        String packaging = currentProject.getPackaging();
 
         // 只针对 war 类型的模块
-        if (!"war".equals(currentProject.getPackaging())) {
+        if (!"war".equals(packaging)) {
+            logger.info(" {} packaging is not war ({}), This extension will Skip ", projectName, packaging);
             return;
         }
 
         // 该功能是否被跳过
-        String skipConfig = currentProject.getProperties().getProperty(SKIP_PROPERTIES, "false");
-        if ("true".equalsIgnoreCase(skipConfig)) {
+        if (shouldSkip(currentProject.getProperties())) {
+            logger.info("{} <{}>true</{}>,  will Skip", projectName, skipProperties(), skipProperties());
             return;
         }
 
-
-        String projectName = currentProject.getName();
-        System.out.println("========== Packaging Is War: " + projectName + ", Will Skip install & deploy ==========");
+        logger.info(" {} packaging is war, will skip install & deploy", projectName);
 
         currentProject.getProperties().setProperty("maven.deploy.skip", "true");
         currentProject.getProperties().setProperty("maven.install.skip", "true");
